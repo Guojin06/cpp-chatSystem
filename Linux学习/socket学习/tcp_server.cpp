4b54bd5cc6bfb605f17718a3e0518d42ae470b27
GuoJin <3039128206@qq.com>
2025-10-24 01:50:48 +0800

    完成Day1 socket学习
    - tcp_server/client代码能跑通
    - 支持循环接受多个客户端
    - 写完学习笔记

--- Linux学习/socket学习/tcp_server.cpp
+++ Linux学习/socket学习/tcp_server.cpp
@@ -32,31 +32,34 @@ int main(){
     return 1;
    }
    printf("listen success，开始监听端口：%d\n", ntohs(addr.sin_port));
-   //4. 接受连接（accept）
-   printf("等待客户端连接...\n");
+   while(1){
+    //4. 接受连接（accept）
+    printf("等待客户端连接...\n");
 
-   struct sockaddr_in client_addr;
-   socklen_t client_addr_len = sizeof(client_addr);//客户端地址长度
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);//客户端地址长度
 
-   int client_sock = accept(listen_sock, (struct sockaddr*)&client_addr, &client_addr_len);//accept返回一个新连接的socket文件描述符
-   if(client_sock < 0){
-    perror("accept failed");
-    return 1;
-   }
+    int client_sock = accept(listen_sock, (struct sockaddr*)&client_addr, &client_addr_len);//accept返回一个新连接的socket文件描述符
+    if(client_sock < 0){
+        perror("accept failed");
+        return 1;
+    }
 
-   //处理连接（recv/send）
+    //处理连接（recv/send）
 
-   //5.接收消息
-   char buf[1024];
-   ssize_t n = recv(client_sock, buf, sizeof(buf)-1, 0);
-   if (n > 0) {
-       buf[n] = '\0';  // 字符串结尾
-       printf("5. 收到客户端消息：%s\n", buf);
-       //6.回复消息
-       const char* reply = "Hello, Client! 我收到你的消息了！";
-       send(client_sock, reply, strlen(reply), 0);
-       printf("6. 回复客户端：%s\n", reply);
-   }
+    //5.接收消息
+    char buf[1024];
+    ssize_t n = recv(client_sock, buf, sizeof(buf)-1, 0);
+    if (n > 0) {
+        buf[n] = '\0';  // 字符串结尾
+        printf("5. 收到客户端消息：%s\n", buf);
+        //6.回复消息
+        const char* reply = "Hello, Client! 我收到你的消息了！";
+        send(client_sock, reply, strlen(reply), 0);
+        printf("6. 回复客户端：%s\n", reply);
+    }
+    close(client_sock);
+}
    
    //6. 关闭连接（close）
    close(listen_sock);
